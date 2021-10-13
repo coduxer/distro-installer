8fe48e5a6c4a64c166baa5c52c3de8196f354532
coduxer <coduxer@gmail.com>
2021-10-13 21:58:18 +0300

    update vb-guestaddition

--- vb-guestaddition-install.sh
+++ vb-guestaddition-install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+set -e
 if ! [ -x "$(command -v 7z)" ]; then
     echo '7z is not installed, installing...' 
     sudo apt-get install p7zip-full
 fi
 
 VB_LATEST_VERSION=`curl -s http://download.virtualbox.org/virtualbox/LATEST.TXT`
+VB_ISO_FILE_NAME="VBoxGuestAdditions_$VB_LATEST_VERSION.iso"
 TMP_DIR=`mktemp -d`
+ISO_TMP_DIR='/tmp/VBoxGuestAdditions'
 EXTRACT_DIR="$TMP_DIR/iso-output"
+mkdir "$ISO_TMP_DIR" || echo hi
 mkdir "$EXTRACT_DIR"
 
-wget https://download.virtualbox.org/virtualbox/$VB_LATEST_VERSION/VBoxGuestAdditions_$VB_LATEST_VERSION.iso -P "$TMP_DIR"
-7z x "$TMP_DIR/VBoxGuestAdditions_$VB_LATEST_VERSION.iso" -o$EXTRACT_DIR
+if [ ! -f "$ISO_TMP_DIR/$VB_ISO_FILE_NAME" ]; then
+	wget https://download.virtualbox.org/virtualbox/$VB_LATEST_VERSION/VBoxGuestAdditions_$VB_LATEST_VERSION.iso -P "$ISO_TMP_DIR"
+fi
+7z x "$ISO_TMP_DIR/$VB_ISO_FILE_NAME" -o$EXTRACT_DIR
 echo $EXTRACT_DIR
 chmod +x $EXTRACT_DIR/autorun.sh
+sed -i 's#/bin/read##g' "$EXTRACT_DIR/autorun.sh"
+sed -i 's#read junk##g' "$EXTRACT_DIR/VBoxLinuxAdditions.run"
 $EXTRACT_DIR/autorun.sh
 sleep 2
 PID=`pgrep -f "VirtualBox*"`
-tail --pid=$PID -f /dev/null
+lsof -p $PID +r 1 &>/dev/null
+#tail --pid=$PID -f /dev/null
