471647ebb46d7d7b9663e81ba0c5514933cdfe12
coduxer <coduxer@gmail.com>
2021-11-16 01:54:57 +0200

    fix ohmyzsh

--- apps/ohmyzsh.sh
+++ apps/ohmyzsh.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+if ! [ -x "$(command -v zsh)" ]; then
+    echo '7z is not installed, installing...' 
+    sudo apt-get install zsh
+fi
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
